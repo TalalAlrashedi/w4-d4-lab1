08c74fafaeb9728d5d122a5df798dceb55e65a1f
TalalAlrashedi <TalalAlrashedi24@hotmail.com>
2025-05-07 10:42:15 +0300

    upload

---
+++ main.js
@@ -0,0 +1,11 @@
+let changeColor = document.querySelector(".h1-change-Color")
+changeColor.style.color = "white";
+changeColor.style.backgroundColor = "green";
+
+let intro = document.getElementById("intro")
+
+intro.style.color = "white"
+intro.style.backgroundColor = "black"
+
+let imageTag = document.getElementsByTagName("img")[0]
+imageTag.src = "https://cdn.pixabay.com/photo/2025/04/29/13/22/cityscape-9567180_1280.jpg"
